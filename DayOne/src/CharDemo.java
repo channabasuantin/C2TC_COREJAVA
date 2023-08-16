cb7fa03befb1ced6a17fd782f157b6d695fe685b
channabasu <channabasu@LAPTOP-20AP1E92>
2023-08-17 01:49:56 +0530

    first commit

---
+++ DayOne/src/CharDemo.java
@@ -0,0 +1,13 @@
+
+public class CharDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      char ch='a';
+      System.out.println(ch);
+      
+      char chone=65;
+      System.out.println(chone);
+	}
+
+}
---
+++ DayOne/src/DataTypesDemo.java
@@ -0,0 +1,19 @@
+
+public class DataTypesDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int value=5/3;
+     float valueone=101/33;
+     float valuetwo=101f/33f;
+     double valuethree=101d/33d;
+     long valuefour=101l/33l;
+     System.out.println(value);
+     System.out.println(valueone);
+     System.out.println(valuetwo);
+     System.out.println(valuethree);
+     System.out.println(valuefour);
+     
+	}
+
+}
